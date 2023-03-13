0bee4f2ec236ac4549c7e79cd9cf6f43753a3ff4
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-12 22:53:43 -0300

    03 Aula 5 Spring server

--- curso-spring-cloud/fornecedor/src/test/java/com/pieropan/fornecedor/FornecedorApplicationTests.java
+++ curso-spring-cloud/fornecedor/src/test/java/com/pieropan/fornecedor/FornecedorApplicationTests.java
@@ -1,6 +1,6 @@
 package com.pieropan.fornecedor;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +9,4 @@ class FornecedorApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
