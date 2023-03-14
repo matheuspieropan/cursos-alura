c8c316a7ae1dc9404cf5b76664ea081c4601a6f6
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-14 15:27:43 -0300

    Finalização do curso

--- curso-spring-cloud/fornecedor_new/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ curso-spring-cloud/fornecedor_new/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.fornecedor.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
 import br.com.alura.microservice.fornecedor.service.InfoService;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/info")
 public class InfoController {
-	
+	Logger LOG = (Logger) LoggerFactory.getLogger(InfoController.class);
+
 	@Autowired
 	private InfoService infoService;
 	
 	@RequestMapping("/{estado}")
 	public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
+		LOG.info("Recebido pedido de informações do fornecedor " + estado);
 		return infoService.getInfoPorEstado(estado);
 	}
 }
--- curso-spring-cloud/loja/src/main/java/com/pieropan/loja/service/CompraService.java
+++ curso-spring-cloud/loja/src/main/java/com/pieropan/loja/service/CompraService.java
@@ -4,21 +4,24 @@ import com.pieropan.loja.client.FornecedorClient;
 import com.pieropan.loja.dto.CompraDTO;
 import com.pieropan.loja.dto.InfoPedidoDTO;
 import com.pieropan.loja.model.Compra;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class CompraService {
+    Logger LOG = (Logger) LoggerFactory.getLogger(CompraService.class);
 
     @Autowired
     FornecedorClient fornecedorClient;
 
-    @Autowired
-    DiscoveryClient discoveryClient;
-
     public Compra realizaCompra(CompraDTO compra) {
+        String estado = compra.getEndereco().getEstado();
+        LOG.info("Buscando informaçoes do fornecedor de {}" + estado);
         fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+        LOG.info("Realizando um pedido de ");
         InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
         Compra compraSalva = new Compra();
         compraSalva.setPedidoId(pedido.getId());
