564eca9cefcbc6c566dcb894d665ef195f2c9c50
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-04-05 13:28:35 -0300

    Segunda aula (implementação do jeito errado)

--- curso-padroes-projeto/src/main/java/Main.java
+++ curso-padroes-projeto/src/main/java/Main.java
@@ -1,2 +1,14 @@
+import enums.TipoImpostoEnum;
+import imposto.CalculadoraImposto;
+import loja.Orcamento;
+
+import java.math.BigDecimal;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraImposto calculadora = new CalculadoraImposto();
+        System.out.println(calculadora.calcular(orcamento, TipoImpostoEnum.ICMS));
+    }
 }
--- curso-padroes-projeto/src/main/java/imposto/CalculadoraImposto.java
+++ curso-padroes-projeto/src/main/java/imposto/CalculadoraImposto.java
@@ -1,11 +1,22 @@
 package imposto;
 
+import enums.TipoImpostoEnum;
 import loja.Orcamento;
-
 import java.math.BigDecimal;
 
 public class CalculadoraImposto {
-    public BigDecimal calcular(Orcamento orcamento){
-        return orcamento.getValor().multiply(new BigDecimal("0.1"));
+
+    public BigDecimal calcular(Orcamento orcamento, TipoImpostoEnum tipoImposto) {
+        switch (tipoImposto) {
+            case ICMS -> {
+                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+            }
+            case ISS -> {
+                return orcamento.getValor().multiply(new BigDecimal("0.06"));
+            }
+            default -> {
+                return BigDecimal.ZERO;
+            }
+        }
     }
 }
