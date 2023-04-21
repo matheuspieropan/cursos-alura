7f5a0207e443f23872d857499a6324322499ff8e
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-04-21 02:58:24 -0300

    Implementação do json

--- java-screenmatch-consumindo-ws/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ java-screenmatch-consumindo-ws/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOMDB;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -15,6 +21,8 @@ public class PrincipalComBusca {
                 URI.create("https://www.omdbapi.com/?t=matrix&apikey=b98e26bc")).build();
 
         HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        TituloOMDB titulo = gson.fromJson((String) response.body(), TituloOMDB.class);
+        System.out.println(titulo);
     }
 }
