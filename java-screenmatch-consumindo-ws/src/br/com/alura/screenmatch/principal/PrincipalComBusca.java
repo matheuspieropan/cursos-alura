242a755e2e37227c9e2e54845ad3530c2266ccf5
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-04-24 00:23:14 -0300

    Trabalhando com leitura e escrita de arquivo

--- java-screenmatch-consumindo-ws/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ java-screenmatch-consumindo-ws/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,11 +1,12 @@
 package br.com.alura.screenmatch.principal;
 
-import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.TituloOMDB;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -23,6 +24,16 @@ public class PrincipalComBusca {
         HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         TituloOMDB titulo = gson.fromJson((String) response.body(), TituloOMDB.class);
-        System.out.println(titulo);
+        FileWriter file = new FileWriter("filme.txt");
+        file.write(titulo.toString());
+        file.close();
+        FileReader reader = new FileReader("filme.txt");
+        int data = reader.read();
+        while (data != -1){
+            System.out.print((char) data);
+            data = reader.read();
+        }
+
+        reader.close();
     }
 }
