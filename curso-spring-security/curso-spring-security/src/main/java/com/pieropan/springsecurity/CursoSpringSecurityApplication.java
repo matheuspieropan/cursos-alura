e76de551baf840d54ac5cd7f9ee5938f1f02a350
mamapisouza <Pi204326@>
2023-05-24 21:23:43 -0300

    Curso spring

---
+++ curso-spring-security/curso-spring-security/src/main/java/com/pieropan/springsecurity/CursoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pieropan.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ curso-spring-security/curso-spring-security/src/test/java/com/pieropan/springsecurity/CursoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pieropan.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
